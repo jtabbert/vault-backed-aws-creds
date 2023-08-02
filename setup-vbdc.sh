ecd4c480b247f32580951303e6f31061ce77f709
jtabbert <101141159+jtabbert@users.noreply.github.com>
2023-08-02 17:35:26 -0400

    Update setup-vbdc.sh

--- setup-vbdc.sh
+++ setup-vbdc.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
+# Enable the AWS secrets engine
 vault secrets enable aws
 
+# Configure the AWS secrets engine
 vault write aws/config/root \
     access_key=$AWS_ACCESS_KEY_ID \
     secret_key=$AWS_SECRET_ACCESS_KEY \
     region=us-east-1
 
+# Create an AWS role, assume role does not work in Instruqt.  iam_user must be used, should be fine for demo purposes
 vault write aws/roles/my-role \
     credential_type=iam_user \
     policy_document=-<<EOF
@@ -22,8 +25,10 @@ vault write aws/roles/my-role \
 }
 EOF
 
+# rotate the AWS creds used to configure the secrets engine
 vault write -f aws/config/rotate-root
 
+# Write vault backed dynamic credentials tfc policy to Vault
 vault policy write vbdc-tfc-policy -<<EOF
 # Allow tokens to query themselves
 path "auth/token/lookup-self" {
@@ -46,10 +51,14 @@ path "aws/creds/*" {
 }
 EOF
 
+
+# The jwt auth method is enabled when you run through the instruqt track instructions
 vault write auth/jwt/config \
     oidc_discovery_url="https://app.terraform.io" \
     bound_issuer="https://app.terraform.io"
 
+
+# Create the jwt auth role.  vbdc = vault backed dynamic credentials
 vault write auth/jwt/role/vbdc-tfc-role -<<EOF
 {
   "bound_claims_type": "glob",
