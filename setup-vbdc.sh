176a21d4cd83e0dd0838fe45f1d58b29b3c27308
jtabbert <101141159+jtabbert@users.noreply.github.com>
2023-08-02 13:50:31 -0400

    Update setup-vbdc.sh

--- setup-vbdc.sh
+++ setup-vbdc.sh
@@ -22,6 +22,8 @@ vault write aws/roles/my-role \
 }
 EOF
 
+vault write -f aws/config/rotate-root
+
 vault policy write vbdc-tfc-policy -<<EOF
 # Allow tokens to query themselves
 path "auth/token/lookup-self" {
